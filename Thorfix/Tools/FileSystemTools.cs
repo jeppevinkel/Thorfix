823885a44754efec44a41a806353e6645d73c0f2
Jeppe Beier <jeppevinkel@gmail.com>
2025-02-07 17:11:24 +0100

    Delete obsolete function

--- Thorfix/Tools/FileSystemTools.cs
+++ Thorfix/Tools/FileSystemTools.cs
@@ -43,50 +43,6 @@ public class FileSystemTools
         var files = Directory.GetFiles(RootDirectory.FullName, "*", SearchOption.AllDirectories).Select(it => it.Replace(RootDirectory.FullName, ""));
         return Task.FromResult(string.Join("\n", files));
     }
-
-//     [Function("Apply a patch to a file in the repository")]
-//     public async Task<string> PatchFile([FunctionParameter("Path to the file", true)] string filePath, [FunctionParameter(@"The format of the patches is as following:
-// @@ -1,6 +1,7 @@
-//  Line 3
-// -Line 4
-// +Modified Line 4
-//  Line 5
-//  Line 6
-// +New Line
-//  Line 7
-//  Line 8
-// Where the numbers after @@ - represent the line numbers in the original file and the numbers after + represent the line numbers in the modified file. The context of the file must match exactly with the context in the patch.", true)] string patch)
-//     {
-//         filePath = GetFullPath(filePath);
-//         Console.WriteLine($"Modify the contents of {filePath}");
-//         if (!IsPathAllowed(filePath))
-//         {
-//             return $"Path was outside the allowed root directory ({RootDirectory.FullName})";
-//         }
-//         
-//         try
-//         {
-//             Console.WriteLine("<START>");
-//             Console.WriteLine(await File.ReadAllTextAsync(filePath));
-//             Console.WriteLine("<PATCH>");
-//             Console.WriteLine(patch);
-//             await Patcher.ApplyPatchAsync(
-//                 filePath,
-//                 patch
-//             );
-//             Console.WriteLine("<>");
-//             Console.WriteLine(await File.ReadAllTextAsync(filePath));
-//             Console.WriteLine("<END>");
-//
-//             return $"{filePath} modified successfully.";
-//         }
-//         catch (Exception e)
-//         {
-//             Console.WriteLine($"Error while modifying {filePath}: {e.Message}");
-//             return $"Error while modifying {filePath}: {e.Message}";
-//         }
-//         
-//     }
     
     [Function("Apply a patch to a file in the repository")]
     public async Task<string> ModifyFile([FunctionParameter("Path to the file", true)] string filePath, [FunctionParameter(@"One or more SEARCH/REPLACE blocks following this exact format:
