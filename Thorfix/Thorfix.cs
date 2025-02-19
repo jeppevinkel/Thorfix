d1e6dd37902e4a2fa6585a04b40c3021e315e44e
Jeppe Beier <jeppevinkel@gmail.com>
2025-02-19 19:28:50 +0100

    Fix head ref for deleting branch

--- Thorfix/Thorfix.cs
+++ Thorfix/Thorfix.cs
@@ -373,8 +373,7 @@ public class Thorfix
                                     });
 
                                     // Delete the old branch
-                                    Console.WriteLine($"Deleting old branch {fullPullRequest.Head.Ref}");
-                                    await _github.Git.Reference.Delete(_repoOwner, _repoName, fullPullRequest.Head.Ref);
+                                    await _github.Git.Reference.Delete(_repoOwner, _repoName, $"heads/{fullPullRequest.Head.Ref}");
 
                                     await _github.Issue.Comment.Create(_repoOwner, _repoName, issue.Number,
                                         "[FROM THOR]\n\nContinuous mode: Successfully merged pull request and closed issue. ✅");
